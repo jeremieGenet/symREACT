0d9963f9e73f6fb1a14310c3848690153aaeefb8
jamyjam52 <jamyjam82377@gmail.com>
2019-06-30 19:45:48 +0200

    Installation de l'API platform et mise en place de nos ressources

--- src/Entity/Customer.php
+++ src/Entity/Customer.php
@@ -2,12 +2,34 @@
 
 namespace App\Entity;
 
-use Doctrine\Common\Collections\ArrayCollection;
-use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use ApiPlatform\Core\Annotation\ApiFilter;
+use Doctrine\Common\Collections\Collection;
+use ApiPlatform\Core\Annotation\ApiResource;
+use ApiPlatform\Core\Annotation\ApiSubresource;
+use Doctrine\Common\Collections\ArrayCollection;
+use Symfony\Component\Serializer\Annotation\Groups;
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\OrderFilter;  // is OK
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\SearchFilter; // is OK
+use Symfony\Component\Validator\Constraints as Assert;
+
+
+
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\CustomerRepository")
+ * @ApiResource(
+ *  collectionOperations={"GET", "POST"},
+ *  itemOperations={"GET", "PUT", "DELETE"},
+ *  subresourceOperations={
+ *      "invoices_get_subresource"={"path"="/client/{id}/factures"}
+ *  },
+ *  normalizationContext={
+ *      "groups"={"customers_read"}
+ *  }
+ * )
+ * @ApiFilter(SearchFilter::class, properties={"firstName":"partial", "lastName", "company"})
+ * @ApiFilter(OrderFilter::class)
  */
 class Customer
 {
@@ -15,36 +37,51 @@ class Customer
      * @ORM\Id()
      * @ORM\GeneratedValue()
      * @ORM\Column(type="integer")
+     * @Groups({"customers_read", "invoices_read"})
      */
     private $id;
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Groups({"customers_read", "invoices_read"})
+     * @Assert\NotBlank(message="Le prénom du client est obligatoire")
+     * @Assert\Length(min=3, minMessage="Le prénom doit faire entre 3 et 255 caractères", max=255, minMessage="Le prénom doit faire entre 3 et 255 caractères")
      */
     private $firstName;
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Groups({"customers_read", "invoices_read"})
+     * @Assert\NotBlank(message="Le prénom du client est obligatoire")
+     * @Assert\Length(min=3, minMessage="Le nom doit faire entre 3 et 255 caractères", max=255, minMessage="Le nom doit faire entre 3 et 255 caractères")
      */
     private $lastName;
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Groups({"customers_read", "invoices_read"})
+     * @Assert\NotBlank(message="L'email du client est obligatoire")
+     * @Assert\Email(message="le format de l'adresse email doit être valide")
      */
     private $email;
 
     /**
      * @ORM\Column(type="string", length=255, nullable=true)
+     * @Groups({"customers_read", "invoices_read"})
      */
     private $company;
 
     /**
      * @ORM\OneToMany(targetEntity="App\Entity\Invoice", mappedBy="customer")
+     * @Groups({"customers_read"})
+     * @ApiSubresource
      */
     private $invoices;
 
     /**
      * @ORM\ManyToOne(targetEntity="App\Entity\User", inversedBy="customers")
+     * @Groups({"customers_read"})
+     * @Assert\NotBlank(message="L'utilisateur est obligatoire")
      */
     private $user;
 
@@ -52,6 +89,40 @@ class Customer
     {
         $this->invoices = new ArrayCollection();
     }
+
+    /**
+     * FAIT MAISON
+     * Retourne le calcul du montant total de la collection de facture du client
+     * @Groups({"customers_read"})
+     *
+     * @return float
+     */
+    public function getTotalAmount(): float
+    {
+        // boucle avec 'array_reduce' sur la collection d' "invoices" tranformée en tableau (toArray())
+        // $total est initialisé à 0 en fin de fonction, et $invoice sera incrémenté du montant de la facture à chaque tour de boucle
+        return array_reduce($this->invoices->toArray(), function($total, $invoice){
+            return $total + $invoice->getAmount();
+        }, 0);
+    }
+
+    /**
+     * FAIT MAISON
+     * Retourne le montant total d'impayé (montant total hors factures payées ou annulées)
+     * @Groups({"customers_read"})
+     *
+     * @return float
+     */
+    public function getUnpaidAmount(): float
+    {
+        // boucle avec 'array_reduce' sur la collection d' "invoices" tranformée en tableau (toArray())
+        // $total est initialisé à 0 en fin de fonction, et $invoice sera incrémenté du montant de la facture à chaque tour de boucle
+        // (ternaire) $total est incrémenté de 0 si le status vaut "PAID" ou "CANCELLED" sinon si le status est "SENT" il est incrémenté
+        return array_reduce($this->invoices->toArray(), function($total, $invoice){
+            return $total + ($invoice->getStatus() === "PAID" || $invoice->getStatus() === "CANCELLED" ? 0 : $invoice->getAmount());
+        }, 0);
+    }
+
 
     public function getId(): ?int
     {
